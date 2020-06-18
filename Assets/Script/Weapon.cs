727cdcd0f5daf83e2d8584e280a5a0a897097812
Edisonfb <edison.fbnts@gmail.com>
2020-06-17 22:41:08 -0300

    foi inserido a logica do tempo de tiro usando coroutine

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    IEnumerator shootCoroutine;
+    public bool canShoot = false;
+    public float shootDelay = 1.0f;
+
+    public void EnterShoot()
+    {
+        Debug.Log("entrou");
+        shootCoroutine = ShootTimer();
+        StartCoroutine(shootCoroutine);
+    }
+
+    public void ExitShoot()
+    {
+        Debug.Log("saiu");
+        StopCoroutine(shootCoroutine);
+    }
+
+   IEnumerator ShootTimer()
+    {
+        yield return new WaitForSeconds(shootDelay);
+        
+        Debug.Log("está rodando essa bagaça");
+        Shoot();
+        StartCoroutine(ShootTimer());
+
+        //yield return null;
+        
+       
+    }
+
+    void Shoot()
+    {
+
+    }
+}
